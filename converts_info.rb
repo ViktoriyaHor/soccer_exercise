49333b6686edc76fe3525799c4ec20b19808f271
ViktoriyaHor <vhoroshun.face@gmail.com>
2019-07-03 17:49:40 +0300

    version with /n

--- converts_info.rb
+++ converts_info.rb
@@ -6,7 +6,27 @@ class ConvertsInfo
   end
 
   def to_array
-    p self.info.split(' ')
+    p arr = self.info.gsub("\n", ', ').split(', ')
+    arr.map! do |elem|
+      ar = elem.split(' ')
+      if ar.length > 2
+        num = ar.last
+        a = ar[0...-1].join(' ')
+        [a, num]
+      else
+        ar
+      end
+    end
+    p arr
+    # p Hash[arr.each_slice(2).to_a]
+    # p arr
+
+
   end
+
+  # def to_hash(arr)
+  #
+  #
+  # end
 end
 
